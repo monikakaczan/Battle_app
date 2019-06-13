6c4fa1abb7fcf2890347104f9c644e26fe1c2522
Student <kaczanowskamonika@gmail.com>
2019-06-13 20:54:45 +0100

    expects players to fill in their names

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,6 +1,14 @@
-feature "Batte" do
-  scenario 'Check homepage' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end 
+feature "Battle" do
+  # scenario 'Check homepage' do
+  #   visit '/'
+  #   expect(page).to have_content 'Testing infrastructure working!'
+  # end
+
+  scenario 'expects players to fill in their names' do
+    visit('/')
+    fill_in "player1", with: "Player1"
+    fill_in 'player2', with: "Player2"
+  click_button('Submit')
+    expect(page).to have_content 'Player1 vs. Player2'
+end
+end
